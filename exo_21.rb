5f6c91abc9ac550c090ad68644845fdab6ba84e6
laurent-martinez <martilaurier@gmail.com>
2019-10-01 16:56:39 +0200

    ajout modif 21

--- exo_21.rb
+++ exo_21.rb
@@ -5,9 +5,7 @@ nbEspaces = nbEtages - 1
 i = 1
 
 while nbEspaces >= 0
-	puts " " * nbEspaces + "#" * i
-
-	nbEtages -= 1
-	nbEspaces -= 1
-	i += 1
+puts " " * nbEspaces + "#" * i
+nbEspaces -= 1
+i += 1
 end
